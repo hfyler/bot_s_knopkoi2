706fe42f276e55e8dda82ab991e7faa0f3c0b91b
hfyler <keltmankirill@gmail.com>
2024-04-03 14:18:42 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -17,17 +17,30 @@ async def main():
 
 @dp.message(Command('кнопки'))
 async def knopki(message: Message):
-    builder.button(
-        text = "Тыкай сюда",
-        callback_data = "tiknul"
-    )
+    builder.button(text = "Тыкай сюда", callback_data = "tiknul")
+    builder.button(text = "Чё?", callback_data = "che")
+    builder.button(text = "А?", callback_data = "aa")
+    builder.button(text = "Кнопка в кнопке, это че", callback_data = "kp_in_kp")
+    builder.button(text = "Ютуб втф, ты итак можешь??", url = "https://www.youtube.com/")
+    builder.adjust(2)
     await message.answer('Тыкай на кнопку', reply_markup=builder.as_markup())
 
 @dp.callback_query(F.data == 'tiknul')
 async def pidor(callback: CallbackQuery):
     await callback.message.answer('Ты пидор!')
 
+@dp.callback_query(F.data == 'che')
+async def pidor(callback: CallbackQuery):
+    await callback.message.answer('Хуй  через плечо11')
 
+@dp.callback_query(F.data == 'aa')
+async def pidor(callback: CallbackQuery):
+    await callback.message.answer('Хуй на!')
+
+@dp.callback_query(F.data == 'kp_in_kp')
+async def dalshe(callback: CallbackQuery):
+    builder.button(text = "Дальше", callback_data = "kp_in_kp2")
+    await callback.message.answer('Дальше?', reply_markup=builder.as_markup())
 
 if __name__ == "__main__":
     asyncio.run(main())
